983836bd2829caa5603fd3110b70de76547e8662
Bad Boy <ri@ri.bad>
2022-10-01 18:31:27 +0300

    aesthetic experiments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import logo from './logo_c.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header background-image">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
