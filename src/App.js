bd5540ec98cb30a21c8f24de3fbe7f007c8588e1
Bad Boy <ri@ri.bad>
2022-10-19 18:21:25 +0300

    poppins

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import text from './text_a.svg';
+// import alchemistLogo from './alchemistLogo.png';
+import alchemistLogo from './alchemist.svg';
 import sound from './solar_flare_sound.mp3'
 import './App.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
 
 function App() {
   const [enterRoomStyle, setEnterRoomStyle] = useState({});
@@ -23,6 +29,12 @@ function App() {
       </header>
       <div className="text-logo" >
         <img src={text} alt="text" />
+        <div style={{display: "flex", width: "276px", justifyContent: "center", alignItems: "center"}}>
+          <div style={{color: "#ffbe2e", fontSize: "23px"}}>the alchemist gallery</div>
+          <img style={{height: "30px", marginLeft: "5px"}} src={alchemistLogo} alt="text" />
+        </div>
+      </div>
+      <div className="alchemist-link" >
       </div>
       <div className="text-instructions">make a circle out of man and woman and draw a traingle on it, and out of a triangle, make again a circle and so you will have your Philosophers Stone</div>
       <div className="enter-subtitles" style={{display: enterRoomStyle.display}}>coming soon</div>
